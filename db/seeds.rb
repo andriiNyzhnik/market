6a20673b28eccda666e989ac54c32f5b13b23cc7
AndriiNyzhnyk <Nyzhnyk@fidor.com>
2019-01-27 17:45:35 +0200

    Use factory bot in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'factory_bot_rails'
+require 'faker'
 
-User.create!(email: 'valer4uk95@gmail.com', first_name: 'Valera', last_name: 'Bobr', phone: '0564535956')
+FactoryBot.create(:user)
 
 # brands
-logitech = Brand.create!(name: 'Logitech')
-halytska_zdoba = Brand.create!(name: 'Галицька здоба')
-molokia = Brand.create!(name: 'Молокія')
+logitech = FactoryBot.create(:brand, name: 'Logitech')
+halytska_zdoba = FactoryBot.create(:brand, name: 'Галицька здоба')
+molokia = FactoryBot.create(:brand, name: 'Молокія')
 
 # product types
-food_type = Category.create!(name: Categories::FOOD)
-computer_type = Category.create!(name: Categories::COMPUTER)
+food_type = FactoryBot.create(:category, name: Categories::FOOD)
+computer_type = FactoryBot.create(:category, name: Categories::COMPUTER)
 mouse_type = computer_type.child_categories.create!(name: Categories::MOUSE)
 keyboard_type = computer_type.child_categories.create!(name: Categories::KEYBOARD)
 
 # products
-mouse = Product.create!(name: 'pro2', category: mouse_type, color: 'white', price: 5.67, brand: logitech, description: 'bla')
-keyboard = Product.create!(name: 'pro3', category: keyboard_type, color: 'white', price: 12.35, brand: logitech, description: 'bla')
-bread = Product.create!(name: 'Висівковий', category: food_type, color: 'gray', price: 1.25, brand: halytska_zdoba, description: 'bla')
-milk = Product.create!(name: 'default', category: food_type, color: 'white', price: 2.25, brand: molokia, description: 'bla')
+Product.create!(name: 'pro2', category: mouse_type, color: Colors::WHITE, price: 5.67, brand: logitech, description: 'bla')
+Product.create!(name: 'pro3', category: keyboard_type, color: Colors::WHITE, price: 12.35, brand: logitech, description: 'bla')
+Product.create!(name: 'Висівковий', category: food_type, color: Colors::GREEN, price: 1.25, brand: halytska_zdoba, description: 'bla')
+Product.create!(name: 'default', category: food_type, color: Colors::WHITE, price: 2.25, brand: molokia, description: 'bla')
 
